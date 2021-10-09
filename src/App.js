cee6d91e20424ac1292f392053d28af4a1ce1a1f
unknown <vladancupric@gmail.com>
2021-10-09 19:46:15 +0200

    New commits

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import {Route, BrowserRouter } from "react-router-dom";
 import AccountsTable from "./components/AccountsTable/AccountsTable";
+import AddAccount from "./components/AddAccount/AddAccount";
 
 class App extends Component {
   state = {
     accounts:[
-      {id:1, name:"Vladan",lastname:"Cupric",phone:"11-11-11", email:"vladancupric@gmail.com"},
-      {id:2, name:"Ranko",lastname:"Stajic",phone:"22-11-11", email:"rankostajic@gmail.com"},
-      {id:3, name:"Branko",lastname:"Riznic",phone:"33-11-11", email:"brankoriznic@gmail.com"}
+      {id:1, name:"Vladan",lastName:"Cupric",phone:"11-11-11", email:"vladancupric@gmail.com"},
+      {id:2, name:"Ranko",lastName:"Stajic",phone:"22-11-11", email:"rankostajic@gmail.com"},
+      {id:3, name:"Branko",lastName:"Riznic",phone:"33-11-11", email:"brankoriznic@gmail.com"}
     ]
+  }
+  addNewAccountToState =(acc)=>{
+this.setState({
+  accounts:[...this.state.accounts,acc]
+})
   }
   render() {
     return (
       <BrowserRouter>
         <Header />
-        <Route to="/">
+        <Route path="/" exact>
           <AccountsTable accounts ={this.state.accounts} />
-
         </Route>
+        <Route path="/add">
+          <AddAccount addNewAccountToState= {this.addNewAccountToState} />
+        </Route>
+
       </BrowserRouter>
     );
   }
--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,7 +5,7 @@ return (
     <tr>
     <td>{account.id}</td>
     <td>{account.name}</td>
-    <td>{account.lastname}</td>
+    <td>{account.lastName}</td>
     <td>{account.phone}</td>
     <td>{account.email}</td>
     </tr>
